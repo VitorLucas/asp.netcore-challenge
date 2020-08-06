81603303aa8b16a176709b1e0ececbd4329c255c
Vitor Santos <vitorlucas013@gmail.com>
2020-08-06 04:12:28 -0300

    Data persistence into InMemoryDatabase

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using challenge.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace challenge.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<CartItem> CartItems { get; set; }
+    }
+}
