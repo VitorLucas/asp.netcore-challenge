dc2c665115e30d3d5f58612e1fab762d6906711a
Vitor Santos <vitorlucas013@gmail.com>
2020-08-06 04:09:44 -0300

    Adding API controllers

---
+++ Controllers/CartController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using challenge.Data;
+using challenge.models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace challenge.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+
+        public async Task<ActionResult<List<CartItem>>> Get([FromServices] DataContext context)
+        {
+            var cartItems = await context.CartItems.ToListAsync();
+            return cartItems;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+
+        public async Task<ActionResult<CartItem>> GetById([FromServices] DataContext context, int id)
+        {
+            try
+            {
+                var cartItem = await context.CartItems
+               .Include(db => db.Product)
+               .AsNoTracking()
+               .FirstOrDefaultAsync(prod => prod.ProductId == id);
+
+                return cartItem;
+            }
+            catch (Exception)
+            {
+                return BadRequest("Item não encontrado");
+            }
+           
+        }
+
+
+        [HttpPost]
+        [Route("")]
+
+        public async Task<ActionResult<CartItem>> Post(
+                    [FromServices] DataContext context,
+                    [FromBody] CartItem model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (context.Products
+                            .AsNoTracking()
+                            .FirstOrDefaultAsync(prod => prod.id == model.ProductId).Result.Quantity > model.Quantity)
+                {
+                    context.CartItems.Add(model);
+                    await context.SaveChangesAsync();
+                    return model;
+                }
+                else 
+                {
+                    return BadRequest("Quantidade indisponivel");
+                }
+                
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+
+        [HttpPut]
+        [Route("")]
+        public async Task<ActionResult<CartItem>> Put([FromServices] DataContext context, [FromBody] CartItem model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                context.CartItems.Update(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult<CartItem>> Delete([FromServices] DataContext context, int? id)
+        {
+            try
+            {
+                var product = await context.CartItems.FindAsync(id);
+                context.CartItems.Remove(product);
+                await context.SaveChangesAsync();
+                return product;
+            }
+            catch (Exception)
+            {
+                return BadRequest("Erro ao deletar produto");
+            }
+
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using challenge.Data;
+using challenge.models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace challenge.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+
+        public async Task<ActionResult<List<Product>>> Get([FromServices] DataContext context)
+        {
+            var products = await context.Products.Include(product => product.Category).ToListAsync();
+            return products;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+
+        public async Task<ActionResult<Product>> GetById([FromServices] DataContext context, int id)
+        {
+            var products = await context.Products
+                .Include(product => product.Category)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(prod => prod.id == id);
+
+            return products;
+        }
+
+
+        [HttpGet]
+        [Route("categories/{id:int}")]
+
+        public async Task<ActionResult<List<Product>>> GetByCategory([FromServices] DataContext context, int id)
+        {
+            var products = await context.Products
+                .Include(product => product.Category)
+                .AsNoTracking()
+                .Where(prod => prod.CategoryId == id)
+                .ToListAsync();
+
+            return products;
+        }
+
+        [HttpPost]
+        [Route("")]
+
+        public async Task<ActionResult<Product>> Post(
+                    [FromServices] DataContext context,
+                    [FromBody] Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Products.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+
+        [HttpPut]
+        [Route("")]
+        public async Task<ActionResult<Product>> Put( [FromServices] DataContext context, [FromBody] Product model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                context.Products.Update(model);
+                await context.SaveChangesAsync();
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> Delete( [FromServices] DataContext context, int? id)
+        {
+            try
+            {
+                var employee = await context.Products.FindAsync(id);
+                context.Products.Remove(employee);
+                await context.SaveChangesAsync();
+                return employee;
+            }
+            catch (Exception)
+            {
+
+                return BadRequest("Erro ao deletar item");
+            }
+           
+        }
+    }
+}
