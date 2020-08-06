c5d7521e78f28930b075367b1aab3e0643a99303
Vitor Santos <vitorlucas013@gmail.com>
2020-08-06 04:12:56 -0300

    Project Models

---
+++ models/CartItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace challenge.models
+{
+    public class CartItem
+    {
+        [Key]
+
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "Categoria inválida")]
+
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatorio")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantidade minima de 1 produto")]
+
+        public int Quantity { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
---
+++ models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace challenge.models
+{
+    public class Category
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatorio")]
+        [MaxLength(60, ErrorMessage ="Este campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage ="Este campo deve conter entre 3 e 60 caracteres")]
+
+        public string Title { get; set; }
+    }
+}
